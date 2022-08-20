658446b6b319a50a8803c6d3fd69a0345e245787
Bikram khatri <100035404+parichakra@users.noreply.github.com>
2022-08-20 20:45:34 +0545

    Create hangman_project_day7.py

---
+++ hangman_project_day7.py
@@ -0,0 +1,144 @@
+
+import random 
+stages = [
+
+"""
+    ________
+    |/   |     
+    |   (_)    
+    |   /|\           
+    |    |        
+    |   / \        
+    |               
+    |___           
+    HANGMAN"""
+
+"""
+  ________                   
+    |/   |                         
+    |   (_)                      
+    |   /|\                             
+    |    |                          
+    |   /                            
+    |                                  
+    |___                              
+    HANGMA""",   
+
+
+"""
+  _________              
+    |/   |                     
+    |   (_)                     
+    |   /|\                    
+    |    |                       
+    |                             
+    |                            
+    |___                          
+    HANGM""",
+"""
+   ________               
+    |/   |                   
+    |   (_)                  
+    |   /|\                    
+    |    |                    
+    |                           
+    |                            
+    |___                    
+    HAN""",
+
+
+"""
+   _________             
+    |/   |               
+    |   (_)                   
+    |    |                     
+    |    |                    
+    |                        
+    |                          
+    |___                          
+    HANG""",
+
+
+"""
+   _________              
+    |/   |                     
+    |   (_)                     
+    |                       
+    |                          
+    |                             
+    |                            
+    |___                          
+    HANGM""",
+
+
+
+"""
+   _________
+    |/   |      
+    |              
+    |                
+    |                 
+    |               
+    |                   
+    |___                 
+    H""",
+
+
+"""
+   _________
+    |/        
+    |              
+    |                
+    |                 
+    |               
+    |                   
+    |___                 
+    """]
+
+ 
+from list import word_list
+choosen_word =random.choice(word_list)
+
+print(f"the word is {choosen_word}")
+
+wordlen =len(choosen_word) 
+
+lives = 6
+display = []
+for letter in range(wordlen):
+    display += "_"
+print(display)      
+
+
+end_of_game = False
+while not end_of_game:
+    guess = input("Guess a letter:").lower()
+
+   
+
+    if guess in display:
+        print(f"you've already guessed{guess}")
+
+    for position in range(wordlen):
+        letter =choosen_word[position]
+        #print(f"your current position:{position} and current letter:{letter} and guess{guess}")
+        if letter ==guess:
+            display[position] =letter
+
+    if guess not in choosen_word:
+        print(f"you guess {guess},that is not in the word, you lose a life")
+        lives -=1
+        if lives ==0:
+            end_of_game = True
+            print("you lose") 
+
+
+
+    print(f"{' '.join(display)}")
+
+    if "_" not in display:
+        end_of_game = True
+        print("you win..")    
+
+
+    print(stages[lives])        
