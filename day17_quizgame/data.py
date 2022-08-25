977befde7ebc64c27895865e7e3b6b46d3d6c14a
Bikram khatri <100035404+parichakra@users.noreply.github.com>
2022-08-25 13:58:54 +0545

    Create data.py

---
+++ day17_quizgame/data.py
@@ -0,0 +1,11 @@
+question_data = [
+    {"text": "A slug's blood is green.", "answer": "True"},
+    {"text": "The loudest animal is the African Elephant.", "answer": "False"},
+    {"text": "Approximately one quarter of human bone  are in the feet .", "answer": "True"},
+    {"text": "The total surface area of human lunge is the size of the football pitch .", "answer": "True"},
+    {"text": "In west Virginia, USA, if you accidentally hit an animal with your car "
+             "your are free to take it home to eat.", "answer": "True"},
+    {"text": " It is illegal to pee in the ocean in Portugal .", "answer": "True "},
+    {"text": "You can lead a cow down stair but not  up stair.", "answer": "False"},
+    {"text": "Everest is the highest mountain in the world.", "answer": "True"}
+]
