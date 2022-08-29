78ec3171a570dfdc8ab4cb7d551cfd6e1eb9e3df
Bikram khatri <100035404+parichakra@users.noreply.github.com>
2022-08-29 16:47:16 +0545

    Create etch_a_sketch_app.py

---
+++ day19/etch_a_sketch_app.py
@@ -0,0 +1,14 @@
+from turtle import Turtle, Screen
+
+tim = Turtle()
+screen = Screen()
+
+
+def move_forwards():
+    tim.forward(10)
+def move_backward():
+    tim.backward(10)
+def turn_right():
+    tim.right(10)
+def turn_left():
+    tim.left(10)
