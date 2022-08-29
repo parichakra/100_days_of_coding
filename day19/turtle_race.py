e5d65231c3a70ba0b3f33bf70dc872e29a258c0f
Bikram khatri <100035404+parichakra@users.noreply.github.com>
2022-08-29 16:50:06 +0545

    Create turtle_race.py
    This a fun game where you can choosa random color of Turtle which will compete with other turtles, check either you will won or not

---
+++ day19/turtle_race.py
@@ -0,0 +1,38 @@
+
+from turtle import Turtle, Screen
+import random
+
+is_race_on = False
+screen = Screen()
+screen.setup(width=500, height=400)
+user_bet = screen.textinput(title="make your bet", prompt="which turtle  will win the race? enter colour:")
+color = ["red", "orange", "yellow", "green", "blue", "purple"]
+y_position = [-70, -40, -10, 20, 50, 80]
+all_turtle = []
+
+for turtle_index in range(0, 6):
+    new_turtle = Turtle(shape="turtle")
+    new_turtle.penup()
+    new_turtle.color(color[turtle_index])
+    new_turtle.goto(x=-230, y=y_position[turtle_index])
+    all_turtle.append(new_turtle)
+
+if user_bet:
+    is_race_on = True
+
+while is_race_on:
+
+
+    for turtle in all_turtle:
+        if turtle.xcor() > 230:
+            is_race_on = False
+            winning_color = turtle.pencolor()
+            if winning_color == user_bet:
+                print(f"you've won! The {winning_color} turtle won the race")
+            else:
+                print(f"you've lose! The {winning_color} turtle won the race")
+
+        random_distant = random.randint(0, 10)
+        turtle.forward(random_distant)
+
+screen.exitonclick()
